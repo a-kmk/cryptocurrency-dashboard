2e5a9bd123900418ee47b3aab884380760cdbc46
abbeyisrael <77085394+abbeyisrael@users.noreply.github.com>
2021-05-08 13:53:43 -0400

    Welcome prop

--- src/App/index.js
+++ src/App/index.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
 
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
 class App extends Component {
   render() {
     return <div className="App"> Hello World!</div>;
