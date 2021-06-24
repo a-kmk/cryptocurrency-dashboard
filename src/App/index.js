fb57a314386d92f7e416b77e84da18b46ab54776
a-kmk <annakmieciak12@gmail.com>
2021-06-23 21:56:00 -0400

    Rolled back

--- src/App/index.js
+++ src/App/index.js
@@ -1,26 +1,10 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 import "./App.css";
-import AppLayout from "./AppLayout";
-import AppBar from "./AppBar";
-import { AppProvider } from "./AppProvider";
-import Settings from "../Settings";
-import Content from "../Shared/Content";
-import Dashboard from "../Dashboard";
+import WelcomeMessage from "./WelcomeMessage";
 
 class App extends Component {
   render() {
-    return (
-      <AppLayout>
-        <AppProvider>
-          <AppBar />
-          <Content>
-            <Settings />
-            <Dashboard />
-          </Content>
-        </AppProvider>
-      </AppLayout>
-    );
+    return <WelcomeMessage />;
   }
 }
 
