78f811342f342ecdd613235e550a92081497dcbf
a-kmk <annakmieciak12@gmail.com>
2021-06-24 09:55:10 -0400

    Added styling

---
+++ src/App/AppBar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+const Logo = styled.div`
+  font-size: 1.5em;
+`;
+
+const Bar = styled.div`
+  display: grid;
+  margin-bottom: 40px;
+  grid-template-columns: 180px 100px 100px;
+`;
+
+const ControlButtonElem = styled.div`
+  cursor: pointer;
+  ${(props) =>
+    props.active &&
+    css`
+      color: blue;
+    `}
+`;
+
+function toProperCase(lower){
+  return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
+
+function ControlButton({ name, active }) {
+  return <ControlButtonElem active={active}>
+  {toProperCase(name)}
+  </ControlButtonElem>;
+}
+export default function () {
+  return (
+    <Bar>
+      <Logo> Logo </Logo>
+      <ControlButton active name="dashboard" />
+      <ControlButton name="settings" />
+    </Bar>
+  );
+}
