c770c11179be26c6c2513def4fcfac5042871138
a-kmk <annakmieciak12@gmail.com>
2021-06-26 18:44:02 -0400

    Add first provider/consumer

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled, { css } from "styled-components";
-
+import { AppContext } from "./AppProvider";
 const Logo = styled.div`
   font-size: 1.5em;
 `;
@@ -20,14 +20,20 @@ const ControlButtonElem = styled.div`
     `}
 `;
 
-function toProperCase(lower){
+function toProperCase(lower) {
   return lower.charAt(0).toUpperCase() + lower.substr(1);
 }
 
 function ControlButton({ name, active }) {
-  return <ControlButtonElem active={active}>
-  {toProperCase(name)}
-  </ControlButtonElem>;
+  return (
+    <AppContext.Consumer>
+      {({ page, setPage }) => (
+        <ControlButtonElem active={page === name} onClick={() => setPage(name)}>
+          {toProperCase(name)}
+        </ControlButtonElem>
+      )}
+    </AppContext.Consumer>
+  );
 }
 export default function () {
   return (
---
+++ src/App/AppProvider.js
@@ -0,0 +1,23 @@
+// state manager of the app
+import React from "react";
+
+export const AppContext = React.createContext();
+
+export class AppProvider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: "settings", //change to dashboard
+      setPage: this.setPage
+    };
+  }
+  setPage = (page) => this.setState({ page });
+
+  render() {
+    return (
+      <AppContext.Provider value={this.state}>
+        {this.props.children}
+      </AppContext.Provider>
+    );
+  }
+}
