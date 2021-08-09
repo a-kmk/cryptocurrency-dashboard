a18c29fc660be4a66e47d8458422605938ff8f41
Anna Kmieciak <annakmieciak12@gmail.com>
2021-08-09 15:22:36 -0400

    localStorage and settings

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -7,9 +7,33 @@ export class AppProvider extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      page: "settings", //change to dashboard
-      setPage: this.setPage
+      page: "dashboard", //change to dashboard
+      ...this.savedSettings(),
+      setPage: this.setPage,
+      confirmFavorites: this.confirmFavorites
     };
+  }
+
+  confirmFavorites = () => {
+    this.setState({
+      firstVisit: false,
+      page: "dashboard"
+    });
+
+    localStorage.setItem(
+      "cryptoDash",
+      JSON.stringify({
+        test: "hello"
+      })
+    );
+  };
+
+  savedSettings() {
+    let cryptoDashData = JSON.parse(localStorage.getItem("cryptoDash"));
+    if (!cryptoDashData) {
+      return { page: "settings", firstVisit: true };
+    }
+    return {};
   }
   setPage = (page) => this.setState({ page });
 
---
+++ src/Settings/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import WelcomeMessage from "./WelcomeMessage";
+import ConfirmButtom from "./ConfirmButton";
+
+export default function () {
+  return (
+    <div>
+      <WelcomeMessage /> <ConfirmButtom />
+    </div>
+  );
+}
