307c04e1936bb754d5d133d13f9bdc0b57295a4b
Anna Kmieciak <annakmieciak12@gmail.com>
2021-08-09 15:32:24 -0400

    Fetch from cryptoCompare API

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,6 +1,6 @@
 // state manager of the app
 import React from "react";
-
+const cc = require("cryptocompare");
 export const AppContext = React.createContext();
 
 export class AppProvider extends React.Component {
@@ -13,6 +13,16 @@ export class AppProvider extends React.Component {
       confirmFavorites: this.confirmFavorites
     };
   }
+
+  componentDidMount = () => {
+    this.fetchCoins();
+  };
+
+  fetchCoins = async () => {
+    let coinList = (await cc.coinList()).Data;
+    this.setState({ coinList });
+    console.log(coinList);
+  };
 
   confirmFavorites = () => {
     this.setState({
