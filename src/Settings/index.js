88dd491a7d6d0e6bb810d372dfba63f1b00de2f1
Anna Kmieciak <annakmieciak12@gmail.com>
2021-08-09 21:27:19 -0400

    Added Coin grid

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import WelcomeMessage from "./WelcomeMessage";
 import ConfirmButtom from "./ConfirmButton";
 import Page from "../Shared/Page";
+import CoinGrid from "./CoinGrid";
 
 export default function () {
   return (
     <Page name="settings">
-      <WelcomeMessage /> <ConfirmButtom />
+      <WelcomeMessage />
+      <ConfirmButtom />
+      <CoinGrid />
     </Page>
   );
 }
