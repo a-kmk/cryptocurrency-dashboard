af6744fa20f4950895fcba8236763ff637ebd71d
Anna Kmieciak <annakmieciak12@gmail.com>
2021-08-09 20:58:11 -0400

    Added 'Page' Component

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,6 +1,7 @@
 // state manager of the app
 import React from "react";
 const cc = require("cryptocompare");
+cc.setApiKey('8bdf8c1a889850bfeb355df39c99fcdf0b9a1c55d25d54ed96dea14952b26835')
 export const AppContext = React.createContext();
 
 export class AppProvider extends React.Component {
--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import WelcomeMessage from "./WelcomeMessage";
 import ConfirmButtom from "./ConfirmButton";
+import Page from "../Shared/Page";
 
 export default function () {
   return (
-    <div>
+    <Page name="settings">
       <WelcomeMessage /> <ConfirmButtom />
-    </div>
+    </Page>
   );
 }
