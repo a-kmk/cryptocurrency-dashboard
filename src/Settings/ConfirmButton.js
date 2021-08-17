7cfb8fe2a508b28db04ea7e4595dd65bed196bb5
Anna Kmieciak <annakmieciak12@gmail.com>
2021-08-17 14:17:50 -0400

    create coinCrid

--- src/Settings/ConfirmButton.js
+++ src/Settings/ConfirmButton.js
@@ -1,11 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 import { AppContext } from "../App/AppProvider";
+import { fontSize1, greenBoxShadow, color3 } from "../Shared/Styles.js";
 
 const ConfirmButtonStyled = styled.div`
   margin: 20px;
-  color: green;
+  color: ${color3};
+  ${fontSize1}
+  padding: 5px;
   cursor: pointer;
+  ${greenBoxShadow};
 `;
 
 //centers our grid
