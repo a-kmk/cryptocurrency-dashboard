91caadc31fdd9b5844926cd2fca32428f8c03155
Anna Kmieciak <annakmieciak12@gmail.com>
2021-09-04 20:19:28 -0400

    Most of search function done

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -21,7 +21,8 @@ export class AppProvider extends React.Component {
       addCoin: this.addCoin,
       removeCoin: this.removeCoin,
       isInFavorites: this.isInFavorites,
-      confirmFavorites: this.confirmFavorites
+      confirmFavorites: this.confirmFavorites,
+      setFilteredCoins: this.setFilteredCoins
     };
   }
 
@@ -74,7 +75,7 @@ export class AppProvider extends React.Component {
     return {};
   }
   setPage = (page) => this.setState({ page });
-
+  setFilteredCoins = (filteredCoins) => this.setState({ filteredCoins });
   render() {
     return (
       <AppContext.Provider value={this.state}>
