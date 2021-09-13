0603ef7e72c8ddda23dac4b86691004a1d8c146a
Anna Kmieciak <annakmieciak12@gmail.com>
2021-09-13 18:18:30 -0400

    dashboard name/price of favorites displays

---
+++ src/Dashboard/PriceGrid.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+import { AppContext } from "../App/AppProvider";
+import PriceTile from "./PriceTile";
+
+const PriceGrid = styled.div`
+  display: grid;
+`;
+export default function () {
+  return (
+    <AppContext.Consumer>
+      {({ prices }) => (
+        <PriceGrid>
+          {prices.map((price) => (
+            <PriceTile price={price} />
+          ))}
+        </PriceGrid>
+      )}
+    </AppContext.Consumer>
+  );
+}
